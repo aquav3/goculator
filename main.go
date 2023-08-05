6f5096b7eca789c74f67d260f152fe60092c9932
aquav3 <kyrbynyt@gmail.com>
2023-08-05 19:36:44 +0300

    Added getting input

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func getInput(prompt string) (string, error) {
+    reader := bufio.NewReader(os.Stdin)
+
+    fmt.Print(prompt)
+    input, err := reader.ReadString('\n')
+    if err != nil {
+        return "", err 
+    }
+    input = strings.TrimRight(input, "\n")
+
+    return input, nil
+}
 
 func main() {
-    fmt.Println("Hello world!")
+    input, err := getInput("Enter the math dude: ") 
+    if err != nil {
+        log.Fatalln("Error: ", err)
+    }
+
+    fmt.Println(input)
 }
